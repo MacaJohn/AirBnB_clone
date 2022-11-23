1a97241cb06e1cccb480aa846381c5b3e171fba2
trmrskr <simonalale@gmail.com>
2022-11-23 22:55:46 +0000

    my python cmd

---
+++ practice/py_shell.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import cmd
+import sys
+
+class Simple_shell(cmd.Cmd):
+    def parseline(self, line):
+        ret = cmd.Cmd.parseline(self, line)
+        return ret
+    def emptyline(self):
+        return cmd.Cmd.emptyline(self)
+    def default(self, line):
+        print('default(%s)' % line)
+        return cmd.Cmd.default(self, line)
+    def preloop(self):
+        print("This is before the command line")
+    def postloop(self):
+        print()
+    def do_greet(self, line):
+        if line:
+            print('hello,', line)
+        else:
+            print('hello')
+    def do_EOF(self, line):
+        return True
+    def do_quit(self, line):
+        sys.exit()
+
+if __name__ == "__main__":
+    Simple_shell().cmdloop('Playing with python cmd tool')
